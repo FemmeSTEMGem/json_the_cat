38e6ec4a33f61b893eb848c1d286f5000027b273
FemmeSTEMGem <femmestemgem@gmail.com>
2022-01-20 18:49:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { fetchBreedDescription } = require("./breedFetcher.js");
+
+const args = process.argv.slice(2);
+const breed = args[0];
+
+fetchBreedDescription(breed, (error, description) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(description);
+  }
+});
+
